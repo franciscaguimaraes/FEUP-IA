713bbfa6bd05f9600b2671e0d062eb833cd8d3f7
inessalexandre <up202103343@up.pt>
2024-02-28 16:21:02 +0000

    optimize menus

--- src/menus/MainMenu.py
+++ src/menus/MainMenu.py
@@ -1,35 +1,21 @@
 # Inside MainMenu.py
 import pygame
 import sys
+from .BaseMenu import BaseMenu
+from .InstructionsMenu import InstructionsMenu
 
 
-class MainMenu:
+
+class MainMenu(BaseMenu):
     def __init__(self, screen, screen_width, screen_height):
-        self.screen = screen
-        self.screen_width = screen_width
-        self.screen_height = screen_height
-        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
-        self.font = pygame.font.Font(None, 36)
-        self.black = (0, 0, 0)
-        self.white = (255, 255, 255)
-        self.backColor = (255, 243, 228)
-        self.yellow = (255, 190, 2)
-        self.click = False
-        self.background_image = pygame.image.load('./imgs/menuBackground.png').convert()
-        self.background_image = pygame.transform.scale(self.background_image, (self.screen_width, self.screen_height))
-
-    def draw_text(self, text, font, color, x, y):
-        textobj = font.render(text, 1, color)
-        textrect = textobj.get_rect()
-        textrect.topleft = (x, y)
-        self.screen.blit(textobj, textrect)
+        super().__init__(screen, screen_width, screen_height, './imgs/menuBackground.png')
 
     def run(self):
         while True:
             self.screen.blit(self.background_image, (0, 0))
             mx, my = pygame.mouse.get_pos()
 
-            button_1 = pygame.Rect(70, 500, 250, 50) # posx, posy, largura, altura
+            button_1 = pygame.Rect(70, 500, 250, 50)  # posx, posy, largura, altura
             button_2 = pygame.Rect(70, 560, 250, 50)
             button_3 = pygame.Rect(70, 620, 250, 50)
 
@@ -38,7 +24,8 @@ class MainMenu:
                     self.start_game()
             if button_2.collidepoint((mx, my)):
                 if self.click:
-                    self.high_scores()
+                    instructions_menu = InstructionsMenu(self.screen, self.screen_width, self.screen_height)
+                    instructions_menu.run()
             if button_3.collidepoint((mx, my)):
                 if self.click:
                     pygame.quit()
@@ -48,27 +35,12 @@ class MainMenu:
             pygame.draw.rect(self.screen, self.yellow, button_2)
             pygame.draw.rect(self.screen, self.yellow, button_3)
 
-            self.draw_text('Start Game', self.font, self.backColor, 90, 510)
-            self.draw_text('Instructions', self.font, self.backColor, 90, 570)
-            self.draw_text('Exit', self.font, self.backColor, 90, 630)
+            self.draw_text('Start Game', self.backColor, 90, 510)
+            self.draw_text('Instructions', self.backColor, 90, 570)
+            self.draw_text('Exit', self.backColor, 90, 630)
 
-            self.click = False
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        pygame.quit()
-                        sys.exit()
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    if event.button == 1:
-                        self.click = True
-
-            pygame.display.update()
+            self.handle_events()
+            self.update_display()
 
     def start_game(self):
         print("Start game placeholder")
-
-    def high_scores(self):
-        print("High scores placeholder")
