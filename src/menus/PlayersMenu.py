476d1f8114c1e50929cff1ad3e7ca2f82ce3afac
inessalexandre <up202103343@up.pt>
2024-03-11 11:29:02 +0000

    fixed player menu

--- src/menus/PlayersMenu.py
+++ src/menus/PlayersMenu.py
@@ -13,9 +13,9 @@ class PlayersMenu(BaseMenu):
             self.screen.blit(self.background_image, (0, 0))
             mx, my = pygame.mouse.get_pos()
 
-            button_1 = pygame.Rect(100, 500, 350, 50)  #posx, posy, largura, altura
-            button_2 = pygame.Rect(100, 600, 350, 50)
-            button_3 = pygame.Rect(100, 700, 350, 50)
+            button_1 = pygame.Rect(490, 265, 350, 50)  #posx, posy, largura, altura
+            button_2 = pygame.Rect(490, 385, 350, 50)
+            button_3 = pygame.Rect(490, 505, 350, 50)
             button_4 = pygame.Rect(70, 760, 350, 50)
 
             if button_1.collidepoint((mx, my)):
@@ -31,15 +31,15 @@ class PlayersMenu(BaseMenu):
                 if self.click:
                     running = False
 
-            pygame.draw.rect(self.screen, self.black, button_1)
-            pygame.draw.rect(self.screen, self.black, button_2)
-            pygame.draw.rect(self.screen, self.black, button_3)
+            pygame.draw.rect(self.screen, self.backColor, button_1)
+            pygame.draw.rect(self.screen, self.backColor, button_2)
+            pygame.draw.rect(self.screen, self.backColor, button_3)
             pygame.draw.rect(self.screen, self.orange, button_4)
 
 
-            self.draw_text('Player vs Player', self.backColor, 90, 510)
-            self.draw_text('Player vs Computer', self.backColor, 90, 570)
-            self.draw_text('Computer vs Computer', self.backColor, 90, 630)
+            self.draw_text('Player vs Player', self.orange, 500, 280)
+            self.draw_text('Player vs Computer', self.orange, 500, 400)
+            self.draw_text('Computer vs Computer', self.orange, 500, 520)
             self.draw_text('Back', self.backColor, 90, 770)
 
             self.handle_events()
